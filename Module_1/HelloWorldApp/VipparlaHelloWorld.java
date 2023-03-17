08daa6dc76e456c81929e2effa18c578a29ceb69
vvipparla99 <128109800+vvipparla99@users.noreply.github.com>
2023-03-16 21:07:50 -0500

    assignment 1.2 github setup

---
+++ Module_1/HelloWorldApp/VipparlaHelloWorld.java
@@ -0,0 +1,12 @@
+ @author Venkateswarlu
+ *
+ */
+public class VipparlaHelloWorld {
+
+	static String firstName = "Venkateswarlu";
+	static String lastName= "Vipparla";
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World from "+ firstName +" "+lastName);
+	}
+}
